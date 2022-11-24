6cd488864f57e47556040e9b6e703dd9969d713e
Nathalie  Dune <nathydune@gmail.com>
2022-11-24 20:46:55 +0100

    bandeau home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "../styles/navbar.css";
-import imageC from "../assets/camera.jpg";
+
 
 const Navbar = () => {
     return <div className="navbar">nav
-    <img className="bandeauImage" src={imageC} alt="bandeauImage"/>
     </div>
     ;
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MiniCard from "../components/MiniCard";
 import "../styles/home.css";
+import imageC from "../assets/camera.jpg";
 
 function Home() {
     return (
         <main className="home">
+            <img className="bandeauImage" src={imageC} alt="bandeauImage"/>
             <h1>HOME</h1>
 
             <div className="profil">
