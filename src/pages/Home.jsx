50e7874498118f1583b4dc30b8fc12d3147f2788
AMELIE GUILLOUX <amelie.guilloux@hotmail.fr>
2022-11-24 21:05:06 +0100

    location filter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import MultiRangeSlider from "multi-range-slider-react";
 import "../styles/home.css";
 
 const Home = () => {
@@ -24,20 +25,48 @@ const Home = () => {
         getAllPeople();
 
     }, []);
+    const [filteredLocations, setFilteredLocations] = useState(filtered.sort((a, b) => a.location.country > b.location.country ? 1 : a.location.country < b.location.country ? -1 : 0));
+    const getLocations = (array) => {
+        let filter = array
+        for (let i = 0; i < filter.length - 1; i++) {
+            if (filter[i].location.country === filter[i + 1].location.country) {
+                filter.splice(i, 1)
+            }
+        } return setFilteredLocations(filter);
+    }
+        ;
     const [gender, setGender] = useState('all');
+    const [location, setLocation] = useState('all');
     const filterPeople = () => {
         let genderFiltered = [];
-        if (gender === "all") { return setFiltered(allPeople) } else {
+        let locationFiltered = [];
+        if (gender === 'all' && location === 'all') {
+            setFiltered(allPeople)
+        }
+        else if (gender !== 'all' && location === 'all') {
             for (let i = 0; i < allPeople.length; i++) {
                 if (allPeople[i].gender === gender) {
                     genderFiltered.push(allPeople[i])
                 }
-            } return setFiltered(genderFiltered)
+            } setFiltered(genderFiltered)
+        }
+        else if (gender === "all" && location !== "all") {
+            for (let i = 0; i < filtered.length; i++) {
+                if (filtered[i].location.country === location) {
+                    locationFiltered.push(filtered[i])
+                }
+            }
+            setFiltered(locationFiltered)
+        }
+        if (location !== "all" && gender !== 'all') {
+            for (let i = 0; i < filtered.length; i++) {
+
+            }
         }
     }
     useEffect(() => {
         filterPeople()
-    }, [gender])
+    }, [gender, location])
     return (
         <div>
             <h1>Home</h1>
@@ -48,6 +77,13 @@ const Home = () => {
                         <option value="all">Tous</option>
                         <option value="female">Femmes</option>
                         <option value="male">Hommes</option>
+                    </select>
+                </div>
+                <div className="countryFilter">
+                    <h4>Localisation</h4>
+                    <select onChange={(e) => setLocation(e.target.value)}>
+                        <option value="all">Tous</option>
+                        {filtered.map((people, index) => <option key={index} value={people.location.country}>{people.location.country}</option>)}
                     </select>
                 </div>
             </div>
