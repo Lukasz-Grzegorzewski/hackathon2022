052f94eed3bf35effdc49ca22dd8d03fc37306f9
Nathalie  Dune <nathydune@gmail.com>
2022-11-25 10:44:50 +0100

    footer nathalie

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import "../styles/footer.css"
+import React from 'react';
+import "../styles/footer.css";
+import {AiFillFacebook} from 'react-icons/ai';
+import {BsInstagram} from 'react-icons/bs';
+import {BsTwitter} from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
+        <div>
         <footer className='footer'>
-            Footer
+            <div className='logo'>
+            <Link  to="//fr-fr.facebook.com/">
+                <AiFillFacebook />
+            </Link>
+
+            <Link to="//www.instagram.com/?hl=fr">
+                <BsInstagram/>
+            </Link>
+
+             <Link to="//twitter.com/?lang=fr">
+                <BsTwitter />
+            </Link>
+        </div>
+
+        <div className='name'>
+            <p className='firts'>Amelie Guilloux</p>
+            <p className='firts'>Lukasz Grzegorzewski</p>
+            <p className='firts'>Baptiste Courtin</p> 
+            <p className='firts'>Thibaud Braunt</p>
+            <p className='firts'>Nathalie Dune</p>   
+        </div>
+
         </footer>
+        </div>
     )
 }
 
